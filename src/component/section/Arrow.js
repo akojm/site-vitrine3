6ceeb1ff845b9b8c2caf3460e57a8a43002c62a1
akojm <ako.achats@free.fr>
2024-02-28 09:53:19 +0100

    maj btn scroll

--- src/component/section/Arrow.js
+++ src/component/section/Arrow.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import '../../style/Arrow.css'
 import { IoMdArrowDropup } from "react-icons/io";
+import { useState } from 'react';
+
+
+
 
 const Arrow = () => {
+   
+    const [activeScrollButton, setActiveScollButton]= useState(false);
+   
+    // Function scrollUp //
+    const scrollUp = ()=>{
+     const scrollValue = document.documentElement.scrollTop; 
+      if (scrollValue > 300) {
+         setActiveScollButton(true);  
+      }else{
+        setActiveScollButton(false)      
+      }
+    }
+    
+    const btnScrollUp = ()=>{
+        window.scrollTo({
+            top: 200,
+            behavior : 'smooth',
+          })
+    }
+    
+
+    // Event click //
+    window.addEventListener('scroll', scrollUp, btnScrollUp )
+
     return (
         <div>
-            <span id='btnArrow' className='arrow' ><IoMdArrowDropup /></span>
+            <span onClick={btnScrollUp}  id='btnArrow' className={activeScrollButton? " active " : " "} ><IoMdArrowDropup /></span>
         </div>
     );
 
